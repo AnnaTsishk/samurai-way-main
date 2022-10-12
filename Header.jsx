b515f1292b992261153b203e4629116f376c33fe
AnnaTsishk <05072022novat@gmail.com>
2022-10-12 15:58:50 +0200

    Add files via upload

---
+++ Header.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Header = ()=> {
+    return<header className='header'>
+        <img src='https://w7.pngwing.com/pngs/554/595/png-transparent-armenia-donation-ansvar-service-social-responsibility-social-miscellaneous-text-trademark.png'/>
+    </header>
+}
+export default Header
---
+++ Profile.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Profile = ()=> {
+    return <div className='content'>
+        <div>
+            <img src= 'https://biosfera92.ru/images/stati/ecology.JPG'/>
+        </div>
+        <div>ava+description</div>
+        <div className='ava'>
+            <img src='https://kartinkin.net/uploads/posts/2022-03/1646130613_1-kartinkin-net-p-kartinki-o-smisle-zhizni-1.jpg'/>
+        </div>
+        <div>My posts</div>
+        <div>New posts</div>
+        <div>
+            <div>post 1</div>
+            <div>post 2</div>
+        </div>
+    </div>
+}
+export default Profile;
