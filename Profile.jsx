aee06644af9c2f74da228fb28b045012a4936f7b
AnnaTsishk <05072022novat@gmail.com>
2022-10-17 22:31:38 +0200

    Add files via upload

--- Profile.jsx
+++ Profile.jsx
@@ -1,20 +1,15 @@
 import React from 'react';
+import cl from './Profile.module.css';
+import MyPosts from "./MyPosts/MyPosts";
+
 
 const Profile = ()=> {
-    return <div className='content'>
+    return <div className={cl.content}>
         <div>
             <img src= 'https://biosfera92.ru/images/stati/ecology.JPG'/>
         </div>
         <div>ava+description</div>
-        <div className='ava'>
-            <img src='https://kartinkin.net/uploads/posts/2022-03/1646130613_1-kartinkin-net-p-kartinki-o-smisle-zhizni-1.jpg'/>
+        <MyPosts />
         </div>
-        <div>My posts</div>
-        <div>New posts</div>
-        <div>
-            <div>post 1</div>
-            <div>post 2</div>
-        </div>
-    </div>
 }
 export default Profile;
