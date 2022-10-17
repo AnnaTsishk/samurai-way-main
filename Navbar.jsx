7cd996d9f68d465ae08efee98277c7c33b31735b
AnnaTsishk <05072022novat@gmail.com>
2022-10-17 22:31:17 +0200

    Add files via upload

--- Navbar.jsx
+++ Navbar.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
+import cl from './Navbar.module.css';
 
 const Navbar = ()=> {
-    return <nav className='nav'>
-        <div><a>Profile</a></div>
-        <div><a>Massages</a></div>
-        <div><a>News</a></div>
-        <div><a>Music</a></div>
-        <div><a>Settings</a></div>
-    </nav>
+    return <nav className={cl.nav}>
+      <div className={`${cl.item} ${cl.active}`}><a>Profile</a></div>
+      <div className={`${cl.item} ${cl.active}`}><a>Massages</a></div>
+      <div className={`${cl.item} ${cl.active}`}><a>News</a></div>
+      <div className={`${cl.item} ${cl.active}`}><a>Music</a></div>
+      <div className={`${cl.item} ${cl.active}`}><a>Settings</a></div>
+      </nav>
+
 }
 export default Navbar;
