0cc7aaed860fffc3a061e4e7ac8ba735364fc207
AnnaTsishk <05072022novat@gmail.com>
2022-10-17 22:30:55 +0200

    Add files via upload

--- Header.jsx
+++ Header.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import cl from './Header.module.css';
 
 const Header = ()=> {
-    return<header className='header'>
+    return <header className ={cl.header}>
         <img src='https://w7.pngwing.com/pngs/554/595/png-transparent-armenia-donation-ansvar-service-social-responsibility-social-miscellaneous-text-trademark.png'/>
     </header>
 }
